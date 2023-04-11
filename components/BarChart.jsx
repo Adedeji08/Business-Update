e6e369f14240db6b5d2d3c7bc2e94b5389eb96ae
unknown <ebenoluwadamilola@gmail.com>
2023-04-11 13:34:16 +0100

    first commit

---
+++ components/BarChart.jsx
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from 'react'
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, BarElement, Title,  LinearScale, Tooltip, Legend } from "chart.js";
+
+
+ChartJS.register(CategoryScale, BarElement, Title,  LinearScale, Tooltip, Legend);
+
+const BarChart = () => {
+    const [chartData, setChartData] = useState({
+        datasets: [],
+    })
+
+    const [chartOptions, setChartOptions] = useState({})
+
+    useEffect(() => {
+      setChartData({
+        labels: ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'],
+        datasets: 
+        [
+          {
+            label: 'Sales $',
+            data: [18127, 22201, 19490, 17938, 24182, 17842, 22475],
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgb(53, 162, 235, 0.4',
+          }, 
+      ]
+      })
+      setChartOptions({
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Daily Revenue'
+            }
+        },
+        maintainAspectRatio: false,
+        responsive: true
+    })
+    }, [])
+    
+  return (
+    <>
+    <div  className='w-full md:col-span-2 relative lg:h-[70vh] h-[70vh] m-auto p-43 border rounded-lg bg-white'>
+        <Bar data={chartData} options={setChartOptions} />
+    </div>
+    </>
+  )
+}
+
+export default BarChart
